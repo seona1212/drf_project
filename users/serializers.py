7488f21b62b1577f59b1c124cfe71c998a0ec3bc
seona1212 <ysh00361@gmail.com>
2023-10-07 19:15:02 +0900

    init project

--- users/serializers.py
+++ users/serializers.py
@@ -6,3 +6,11 @@ class Userserializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields= "__all__"
+
+    def create(self, validated_data):
+        user = super().create(validated_data)
+        print(validated_data)
+        password = user.password
+        user.set_passwrd(password)
+        print(user.passwrd)
+        return user
